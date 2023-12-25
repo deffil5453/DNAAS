08ce782afd3cac53ed4db76c933f0bd357a24afb
Benlinni <nozdrin_vitya@mail.ru>
2023-12-25 15:35:24 +0300

    243534

---
+++ DNASS/Pages/CartItems/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DNASS.Pages.CartItems
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
